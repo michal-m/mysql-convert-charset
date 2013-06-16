640d95577b74ace9b44a9d4ef2b7501a9e159b78
Michał Musiał <michal.j.musial@gmail.com>
2013-06-16 17:04:24 +0200

    setup db link and pre-escape query variables

--- mysql-change-charset-and-collation.php
+++ mysql-change-charset-and-collation.php
@@ -1,1 +1,14 @@
 <?php
+header('Content-Type: text/plain');
+
+require 'config.php';
+
+$dbl = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
+if ($dbl->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $dbl->connect_errno . ") " . $dbl->connect_error;
+    exit(1);
+}
+
+$CHARSET    = $dbl->real_escape_string($CHARSET);
+$COLLATION  = $dbl->real_escape_string($COLLATION);
